3cf4f400d3b5d5746a33f532979b39de538ab64b
Mohammad <mohammadhooshmand76@gmail.com>
2018-09-22 14:10:22 +0330

    port added

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
+const port = 3001;
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.listen(3001, () => {
-    console.log('server is running on port 3001!')
+app.listen(port, () => {
+    console.log(`server is running on port ${port}!`)
 })
